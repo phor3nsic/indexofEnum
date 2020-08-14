ba8cce7a07cfd1807b6466c76102787189da44e3
phor3nsic <phorensic@protonmail>
2020-08-14 19:45:37 -0300

    recursive and get paths

--- index_of_enum.py
+++ index_of_enum.py
@@ -1,11 +1,13 @@
 import requests
 import sys
 from bs4 import BeautifulSoup
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
-url = ""
+url = "https://www.smartvivoempresa.com.br/wp-content/uploads/"
 extenxions = ['.txt','.sql','.zip','.rar','.bak','.old','.csv','.xml','.doc','.docx']
-direcs = []
-debug = False
+DIRS = []
+debug = True
 proxy_debug = None
 
 proxy = {
@@ -17,33 +19,47 @@ if debug == True:
 	proxy_debug = proxy
 
 def check():
-	req = requests.get(url, proxies=proxy_debug)
-	if req.status_code == 200:
-		if "Index of" in  req.text:
-			pass
-	else:
+	req = requests.get(url, proxies=proxy_debug, verify=False)
+	if req.status_code == 200 and "Index of" not in  req.text:
 		print("[-]No Index of found!")
 		sys.exit()
 
-def checkExt():
-	req = requests.get(url, proxies=proxy_debug)
+def checkExt(url, path):
+	if path == None:
+		path = ""
+
+	req = requests.get(url+path, proxies=proxy_debug, verify=False)
 	for x in extenxions:
 		if x in req.text:
-			print(f"{url}/{x}")
+			print(f"{url+path}/{x}")
 
-def getContent(url):
-	page = requests.get(url, proxies=proxy_debug)
+def getContent(url,path):
+	global DIRS
+
+	if path == None:
+		path = ""
+
+	page = requests.get(url+path, proxies=proxy_debug, verify=False)
 	soup = BeautifulSoup(page.content, "html.parser")
-	html = list(soup.children)[2]
-	body = list(html.children)[3]
-	tables = list(body.children)[3]
-	# as tables de arquivos comecam no array 7
-	#print(list(tables.children))
+	content = []
+
+	for x in soup.find_all("a"):
+		if x.get("href")[-1] == "/":
+			content.append(x.get("href"))
+	del content[0:1]
+
+	for x in content:
+		DIRS.append(path+x)
 	
 def main():
 	check()
-	checkExt()
-	getContent(url)
+	checkExt(url, None)
+	getContent(url, None)
+	for x in DIRS:
+		checkExt(url, x)
+		getContent(url, x)
+
+	print(DIRS)
 
 if __name__ == '__main__':
 	main()
